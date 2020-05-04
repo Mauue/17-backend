7f9ed294740a511f401f9e9a2766f4d881db510d
Mauue <364791516@qq.com>
2020-05-04 14:06:34 +0800

    add cors

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from controllers import init_app
 from config import config
 from db import db_init_app
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import *
 
 
 def create_app():
     app = Flask(__name__)
-
+    CORS(app, supports_credentials=True)
     app.config.from_mapping(config)
 
     db_init_app(app)
