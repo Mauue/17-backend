be94a5b48d4459c22ef91e6f5729d0ff1fe29644
Mauue <364791516@qq.com>
2020-06-05 21:28:22 +0800

    update session httponly false

--- config/__init__.py
+++ config/__init__.py
@@ -5,6 +5,7 @@ config = {
         DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME),
     "SQLALCHEMY_TRACK_MODIFICATIONS": False,
     "SECRET_KEY": b'IV\xb0\x91]\xd0\xa3V%\x1c\xa0\xe99kt\x1a',
+    "SESSION_COOKIE_HTTPONLY": False,
     "WTF_CSRF_ENABLED": False,
     "REDIS_URL": "redis://:@localhost:6379/0",
     "IM_APP_ID": IMAppID,
